2aad29cb7d09b5fd2c75033a004c784d2d30931c
Nafi Durmuş <nafidurmus07@gmail.com>
2017-07-06 17:30:33 +0300

    spec uygulama

---
+++ string_calculator.rb
@@ -0,0 +1,23 @@
+module StringCalculator
+	def add
+		return 0 if empty?
+		raise_if_negatives_present
+		digit.inject {|sum, x| sum + x}
+	end
+
+	def raise_if_negatives_present
+		raise "Negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+	end
+
+	def negatives
+		@negatives ||= digits.select{|x| x<0}
+	end
+
+	def digits
+		gsup("\n",delimiter).split(delimiter).map {|x| x.to_i }
+	end
+
+	def delimiter
+		@delimiter ||= self [0,2] == '//' ? delimiter = self[2,1] : ','
+	end
+end
